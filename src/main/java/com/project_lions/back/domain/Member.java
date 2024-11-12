7be0a056c1e275707c8238980441ade7f3841a32
IkJuLim <limik1004@gmail.com>
2024-11-12 14:20:17 +0900

    member controller 생성

--- src/main/java/com/project_lions/back/domain/Member.java
+++ src/main/java/com/project_lions/back/domain/Member.java
@@ -2,6 +2,13 @@ package com.project_lions.back.domain;
 
 import com.project_lions.back.domain.common.BaseEntity;
 import com.project_lions.back.domain.enums.Role;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,26 +19,36 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@Entity
 public class Member extends BaseEntity {
 
   //primary Key
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   //email
+  @Column(nullable = false, length = 30, unique = true)
   private String email;
 
   //비밀번호
+  @Column(nullable = false)
   private String password;
 
   //이름
+  @Column(nullable = false)
   private String name;
 
   //전화번호
+  @Column(nullable = false)
   private String phone;
 
   //주소
+  @Column(nullable = false)
   private String address;
 
   //권한 -> USER, ADMIN
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
   private Role role;
 }
