d80e625b83d0d4986d6b14a7889492984cc1e25b
IkJuLim <limik1004@gmail.com>
2024-11-15 21:01:33 +0900

    member findMy controller 구현

--- src/main/java/com/project_lions/back/controller/MemberController.java
+++ src/main/java/com/project_lions/back/controller/MemberController.java
@@ -8,6 +8,7 @@ import com.project_lions.back.domain.dto.MemberResponseDto;
 import com.project_lions.back.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,12 +37,24 @@ public class MemberController {
   @PutMapping
   public ApiResponse<?> update(@RequestBody @Validated MemberRequestDto.Update memberUpdateDto) {
     Member updatedMember = memberService.update(memberUpdateDto);
-    return ApiResponse.of(SuccessStatus.MEMBER_JOIN, MemberResponseDto.SignUpDto.builder()
+    return ApiResponse.of(SuccessStatus.MEMBER_JOIN, MemberResponseDto.UpdateDto.builder()
         .id(updatedMember.getId())
         .email(updatedMember.getEmail())
         .name(updatedMember.getName())
         .phone(updatedMember.getPhone())
         .address(updatedMember.getAddress())
+        .build());
+  }
+
+  @GetMapping
+  public ApiResponse<?> myInfo() {
+    Member myInfo = memberService.findMy();
+    return ApiResponse.of(SuccessStatus.MEMBER_JOIN, MemberResponseDto.MyInfoDto.builder()
+        .id(myInfo.getId())
+        .email(myInfo.getEmail())
+        .name(myInfo.getName())
+        .phone(myInfo.getPhone())
+        .address(myInfo.getAddress())
         .build());
   }
 }
